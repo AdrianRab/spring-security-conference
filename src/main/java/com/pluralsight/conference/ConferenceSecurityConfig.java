a43db8bc253448f63d88bebe6e6aeab230b13b00
Adrian <adrian.rabowski@gmail.com>
2022-11-08 12:05:30 +0100

    Logout logic done

--- src/main/java/com/pluralsight/conference/ConferenceSecurityConfig.java
+++ src/main/java/com/pluralsight/conference/ConferenceSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -45,7 +46,15 @@ public class ConferenceSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .rememberMe()
                 .key("superSecretKey")
-                .tokenRepository(tokenRepository());
+                .tokenRepository(tokenRepository())
+
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout=true")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/perform_logout", "GET"))
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .permitAll();
     }
 
     @Bean
