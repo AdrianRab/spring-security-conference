cf60b5288dab2f1116e070b5d9d38e1b522ba158
Adrian <adrian.rabowski@gmail.com>
2022-11-09 13:19:56 +0100

    Securing UI elements

--- src/main/java/com/pluralsight/conference/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/conference/controller/RegistrationController.java
@@ -2,6 +2,7 @@ package com.pluralsight.conference.controller;
 
 import com.pluralsight.conference.model.Registration;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,17 +15,17 @@ import javax.validation.Valid;
 public class RegistrationController {
 
     @GetMapping("registration")
-    public String getRegistration(@ModelAttribute ("registration")Registration registration) {
+    public String getRegistration(@ModelAttribute("registration") Registration registration) {
         return "registration";
     }
 
     @PostMapping("registration")
     @Secured("USER")
-    public String addRegistration(@Valid @ModelAttribute ("registration")
-                                              Registration registration,
-                                  BindingResult result) {
+    public String addRegistration(@Valid @ModelAttribute("registration") Registration registration, BindingResult result, Authentication auth) {
 
-        if(result.hasErrors()) {
+        System.out.println("Auth: " + auth.getPrincipal());
+
+        if (result.hasErrors()) {
             System.out.println("There were errors");
             return "registration";
         }
